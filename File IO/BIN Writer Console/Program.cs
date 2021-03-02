e1999a4e02d3be6f2a2d512b550c33d5aec7101f
HENDRIKASUS\Hendrik <hendrikdw@outlook.be>
2021-03-02 09:59:50 +0100

    les dinsdag 0203

--- File IO/BIN Writer Console/Program.cs
+++ File IO/BIN Writer Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace BIN_Writer_Console
 {
@@ -7,6 +8,23 @@ namespace BIN_Writer_Console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string path = @"D:\OneDrive\HoGent\test.bin";
+            FileInfo f = new FileInfo(path);
+            using (BinaryWriter bw = new BinaryWriter(f.OpenWrite()))
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    bw.Write(i);
+                }
+            }
+            using (BinaryReader br = new BinaryReader(f.OpenRead()))
+            {
+                while (br.BaseStream.Position != br.BaseStream.Length)
+                {
+                    int x = br.ReadInt32();
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadLine();
         }
     }
-}
---
+++ File IO/DIR Control Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIR_Control_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
