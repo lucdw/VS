b527547e755bd43a4f721744078cbd246f49719b
HENDRIKASUS\Hendrik <hendrikdw@outlook.be>
2021-03-02 10:35:14 +0100

    Start Opdracht Adressen Info

---
+++ AdresInfo/AdresInfoConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdresInfoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AdresInfo/AdresInfoLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdresInfoLib
+{
+    public class Class1
+    {
+    }
+}
